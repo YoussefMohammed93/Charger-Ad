1499bf54975e61fe13b10430ebe7d580eab3d183
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-02-05 21:41:59 +0200

    Edit video

--- components/banner.tsx
+++ components/banner.tsx
@@ -22,6 +22,18 @@ export const Banner = () => {
         توصيل مجاني + تخفيض <span className="text-[#B88E2F]"> 50% </span>الى
         غاية و منتصف الليل
       </motion.p>
+      <motion.div className="flex justify-center mt-12">
+        <motion.video
+          src="/vid.mp4"
+          className="object-cover w-full max-w-2xl h-[400px] sm:h-[500px]"
+          controls
+          autoPlay
+          loop
+          muted
+        >
+          Your browser does not support the video tag.
+        </motion.video>
+      </motion.div>
     </motion.section>
   );
 };
